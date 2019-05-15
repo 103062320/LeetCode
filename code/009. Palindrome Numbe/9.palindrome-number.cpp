4156c85f99dd38bae35857cfc49b0e4a175958a2
joyce <joyce09954@gmail.com>
2019-05-15 14:51:19 +0800

    Create 9.palindrome-number.cpp

---
+++ code/009. Palindrome Numbe/9.palindrome-number.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=9 lang=cpp
+ *
+ * [9] Palindrome Number
+ */
+class Solution {
+public:
+    bool isPalindrome(int x) {
+		// special case 0
+		if(x == 0) return true;
+		
+        if(x < 0 || x % 10 == 0){
+			return false;
+		}
+		else {
+			long origin = x, reverse=0;
+			while(origin) {
+				reverse = reverse * 10 + origin % 10;
+				origin /= 10;
+			}
+			if(reverse == x) return true;
+			else return false;
+		}
+    }
+};
+
