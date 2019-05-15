34f977312d19a69d34e1b9451fe8b4518e29204b
joyce <joyce09954@gmail.com>
2019-05-15 14:57:49 +0800

    Update 9.palindrome-number.cpp

--- code/009. Palindrome Numbe/9.palindrome-number.cpp
+++ code/009. Palindrome Numbe/9.palindrome-number.cpp
@@ -7,20 +7,30 @@ class Solution {
 public:
     bool isPalindrome(int x) {
 		// special case 0
-		if(x == 0) return true;
-		
-        if(x < 0 || x % 10 == 0){
-			return false;
-		}
-		else {
-			long origin = x, reverse=0;
-			while(origin) {
-				reverse = reverse * 10 + origin % 10;
-				origin /= 10;
+		/*
+			if(x == 0) return true;
+
+			if(x < 0 || x % 10 == 0){
+				return false;
 			}
-			if(reverse == x) return true;
-			else return false;
+			else {
+				long origin = x, reverse=0;
+				while(origin) {
+					reverse = reverse * 10 + origin % 10;
+					origin /= 10;
+				}
+				if(reverse == x) return true;
+				else return false;
+			}
+		*/
+		// shorter code
+		long origin = x, reverse = 0;
+		while(origin > 0){
+			reverse = reverse * 10 + origin % 10;
+			origin /= 10;
 		}
+		return x == reverse;
+
     }
 };
 
