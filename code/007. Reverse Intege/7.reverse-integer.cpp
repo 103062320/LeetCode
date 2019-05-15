5ed90837afca8000f7ff10cb4f24ec5b0d6f5322
joyce <joyce09954@gmail.com>
2019-05-15 14:43:10 +0800

    Update 7.reverse-integer.cpp

--- code/007. Reverse Intege/7.reverse-integer.cpp
+++ code/007. Reverse Intege/7.reverse-integer.cpp
@@ -6,17 +6,18 @@
 class Solution {
 public:
     int reverse(int x) {
+		// My method, not good
 		bool neg = false;
-		if(x < -1 * pow(2,31) || x > pow(2,31)-1) return 0;
 		if(x < 0){
 			neg = true;
 		}
-        stringstream ss;
+		stringstream ss;
 		ss << x;
 		string str = ss.str();
 		std::reverse(str.begin(), str.end());
 		int y;
 		try{
+			// seems this will ignore the '-' at the end
 			y = std::stoi(str);
 		}
 		catch(exception e){
@@ -24,6 +25,16 @@ public:
 		}
 		if(neg) y *= -1;
 		return y;
+
+		// Reference : GitHub tim70036
+		/*
+			long reverse=0;
+			while(x) {
+				reverse = reverse * 10 + x % 10;
+				x = x / 10;
+			}
+			return ((reverse > INT32_MAX || reverse < INT32_MIN) ? 0 : reverse);
+		*/
     }
 };
 
